db58df897741d562cef85cbb38a90a48b08d4725
Matt Torok <mtorok@berkeley.edu>
2013-05-02 14:57:46 -0700

    groff: 1.22.2 - fixed deps needed for html gen
    During configuration, groff checks for gs, pnmcut, pnmcrop,
    pnmtopng, psselect and pnmtops. If it does not find them, then the
    html output generator will not work correctly in certain cases.
    
    Homebrew's superenv was preventing these utilities from ever being
    found, even if you did have them installed. I've added ghostscript,
    netpbm and psutils to the deps, which should both ensure that these
    utilties are installed, and that groff can find them.
    
    Additionally, I've patched a bug in the ./configure script when being
    used with homebrew, where a check for pnmtops' capabilities was hanging
    the script. Somehow, the '|' character in the shell command was causing
    the hang. Oddly, the ./configure script works absolutely fine if you run
    it from the shell yourself (either with brew install -i, or by
    downloading the source yourself and building.) No idea what weird voodoo
    interaction between homebrew, configure, and bash was causing it.

--- groff.rb
+++ groff.rb
@@ -6,9 +6,46 @@ class Groff < Formula
   mirror 'http://ftp.gnu.org/gnu/groff/groff-1.22.2.tar.gz'
   sha1 '37223941e25bb504bf54631daaabb01b147dc1d3'
 
+  depends_on 'ghostscript'
+  depends_on 'netpbm'
+  depends_on 'psutils'
+
+  def patches
+    # Fix a bug where ./configure would hang trying to execute a shell command with a '|' in it
+    DATA
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}", "--without-x"
     system "make" # Separate steps required
     system "make install"
   end
 end
+
+
+__END__
+diff --git a/configure b/configure
+index 63e9e5d..6f97736 100755
+--- a/configure
++++ b/configure
+@@ -10440,18 +10440,8 @@ $as_echo "$as_me: WARNING: missing program$plural:
+ 
+ 
+ 
+-{ $as_echo "$as_me:${as_lineno-$LINENO}: checking whether pnmtops can handle the -nosetpage option" >&5
+-$as_echo_n "checking whether pnmtops can handle the -nosetpage option... " >&6; }
+-   if echo P2 2 2 255 0 1 2 0 | pnmtops -nosetpage > /dev/null 2>&1 ; then
+-     { $as_echo "$as_me:${as_lineno-$LINENO}: result: yes" >&5
+-$as_echo "yes" >&6; }
+-     pnmtops_nosetpage="pnmtops -nosetpage"
+-   else
+-     { $as_echo "$as_me:${as_lineno-$LINENO}: result: no" >&5
+-$as_echo "no" >&6; }
+-     pnmtops_nosetpage="pnmtops"
+-   fi
+ 
++pnmtops_nosetpage="pnmtops -nosetpage"
+ 
+     { $as_echo "$as_me:${as_lineno-$LINENO}: checking whether we are using the GNU C Library >= 2.1 or uClibc" >&5
+ $as_echo_n "checking whether we are using the GNU C Library >= 2.1 or uClibc... " >&6; }
+
